7664e0e9cb05d8d715c5ee4c65a2ae24080f93f8
S1mple777 <87543004+S1mple777@users.noreply.github.com>
2025-06-12 17:35:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:river_pod_hooks_example/auth_service.dart';
-import 'package:riverpod/riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
